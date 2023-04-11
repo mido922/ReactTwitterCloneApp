39d4d832b9b96b9720bc273624de63d5eab04304
mido922 <mzohry@gmail.com>
2023-04-11 23:23:12 +0200

    React Social Media First Upload

---
+++ src/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Sidebar.css";
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        Hello
+        <TwitterIcon />
+
+        <SidebarOption />
+
+
+
+
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/SidebarOption.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarOption() {
+  return (
+    <div className="sidebarOption">
+        <h1>Sidebar Option</h1>
+    </div>
+  )
+}
+
+export default SidebarOption
