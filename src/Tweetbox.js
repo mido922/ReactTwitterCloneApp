3a79327494dc3424fba0fa30e60d67ca655f62b0
mido922 <mzohry@gmail.com>
2023-04-22 23:39:35 +0200

    Added the ability to post

--- src/Tweetbox.js
+++ src/Tweetbox.js
@@ -2,25 +2,58 @@ import React from 'react';
 import "./Tweetbox.css";
 import { Avatar, Button } from '@mui/material';
 import { useState } from 'react';
+import {db} from './firebase';
+import {collection, addDoc} from "firebase/firestore";
 
 function Tweetbox() {
 
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = async (e) => {
+    e.preventDefault();  
+   
+    try {
+        const docRef = await addDoc(collection(db, "posts"), {
+          avatar: "",
+          displayName: "Placeholder",
+          image: tweetImage,
+          text: tweetMessage,
+          username: "",
+          verified: false,
+              
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+
+      setTweetMessage("");
+      setTweetImage("");
+}
 
   return (
     <div className="tweetBox">
         <form>
             <div className="tweetBox__input">
-                <Avatar src=""></Avatar>
+                <Avatar src=""/>
                 <input
+                onChange={(e) => setTweetMessage(e.target.value)}
+                  value={tweetMessage}
                   placeholder="What's happening?"
                   type="text" />
                 <input
+                  value={tweetImage}
+                  onChange={(e) => setTweetImage(e.target.value)}
                   className="tweetBox__imageInput"
                   placeholder="Enter image URL"
                   type ="text" 
                 />
             </div>
-            <Button className="tweetBox__tweetButton">Tweet</Button>
+            <Button
+              type="submit"
+              onClick={sendTweet}
+            className="tweetBox__tweetButton">Tweet</Button>
 
         </form>
     </div>
